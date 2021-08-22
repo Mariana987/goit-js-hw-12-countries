d265fc12a0166feebf2a7036507c38b8f87f5de4
Mariana987 <m1a1r1y1a1n1a1@gmail.com>
2021-08-22 18:27:00 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-fetch('https://restcountries.eu/rest/v2/name/ty')
-    .then(response => {
-        return response.json();
-    })
-    .then(country => {
-        console.log(country)
-    })
-    .catch(error => {
-        consolr.log(error)
-    })
+
+import { debounce } from "lodash";
+import searchResults from '../src/search-results.hbs';
+// import countryCards from '../src/country-cards.hbs';
+
+const inputSearch = document.querySelector('[data-action="input-search"]');
+const countryContainer = document.querySelector('.container')
+inputSearch.addEventListener('input', debounce(onSearch, 500))
+
+
+
+function onSearch(e) {
+    e.preventDefault();
+
+    const name = search.value;
+
+    const url = `https://restcountries.eu/rest/v2/name/${name}`;
+
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+            return data
+        })
+
+        .then(appendCountryMarkup)
+
+
+        .catch(error => {
+            console.log(error)
+        });
+
+
+}
+
+function appendCountryMarkup(data) {
+    countryContainer.insertAdjacentHTML('beforeend', searchResults(data))
+    return
+}
