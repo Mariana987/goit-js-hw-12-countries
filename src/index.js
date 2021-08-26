21208c5394fc7968cf24a951bec03b141e971b13
Mariana987 <m1a1r1y1a1n1a1@gmail.com>
2021-08-26 18:43:26 +0200

    .

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import searchResults from '../src/search-results.hbs';
 import countryCard from '../src/country-card.hbs';
 
 const inputSearch = document.querySelector('[data-action="input-search"]');
-const countryContainer = document.querySelector('.container')
-inputSearch.addEventListener('input', debounce(onSearch, 500))
+const countryContainer = document.querySelector('.result_container')
+inputSearch.addEventListener('input', debounce(onSearch, 1000))
 
 
 
@@ -46,22 +46,24 @@ function appendCountryCard(data) {
 function buildingCard(data) {
 
     if (data.length >= 2 && data.length <= 10) {
-        // resetInput()
+        resetInput()
         return appendCountryMarkup(data)
 
     }
     if (data.length === 1) {
+        resetInput()
         return appendCountryCard(data)
     }
     else {
+        resetInput()
         return error('To many countries founded. Please enter a more specific request.')
     }
 }
 
+function resetInput() {
+    return search.value = ''
+}
 
-// function resetInput() {
-//     return search.value = ''
-// }
 function clearContainer() {
     countryContainer.innerHTML = ''
 }
